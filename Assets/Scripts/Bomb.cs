1e198c9b6b114e7a0a77c97e648896d72c1fcaf7
LW260328 <lw260328@falmouth.ac.uk>
2023-03-01 18:41:18 +0000

    dropping bomb in map working, need to add anim

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    //defining input key for placing bombs, player inventory for bombs, and how long it takes for them to detonate
+    public GameObject playerBomb;
+    public KeyCode placeBomb = KeyCode.Space;
+
+    public int bombMax = 1;
+    private int bombsLeft;
+
+    public float bombExplodeTime = 3f;
+
+    private void OnEnable()
+    {
+        bombsLeft = bombMax;
+    }
+
+    private void Update()
+    {
+        //if player has a bomb in their inventory when they press space bar, they drop a bomb on the map 
+        if (bombsLeft > 0 && Input.GetKey(placeBomb))
+           StartCoroutine (DropBomb());
+    }
+
+    private IEnumerator DropBomb()
+    {
+        //drops bomb on next closest whole tile to the player
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        //spawn bomb in map + reduce bombs player has remaining to 0
+        GameObject bomb = Instantiate(playerBomb, position, Quaternion.identity);
+        bombsLeft = 0;
+
+        //waits 3 seconds before continuing to let the bomb explode and do damage
+        yield return new WaitForSeconds(bombExplodeTime);
+
+        //testing if works before adding explosions
+        Destroy(bomb);
+    }
+}
